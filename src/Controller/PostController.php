f49c5d7be6a8965640f1f8a2649170d83738d567
foool1 <julek.pryczkowski@wp.pl>
2025-08-11 12:14:20 +0200

    zaaktualizowanie repozytorium do najnowszej wersjii

---
+++ src/Controller/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+	/**
+	 * @Route("/post", name="post_index")
+	 */
+	public function index(): Response
+	{
+		return new Response('<html><body>Post index</body></html>');
+	}
+}
